9f483f045b99f12c11e1be107f89732f08e74ba2
Masayoshi <masasensei26@mail.com>
2024-01-17 11:54:05 +0700

    unit test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Home - Masayoshi",
+  description: "Home Page",
+  authors: {
+    name: "Masayoshi",
+    url: "https://github.com/MasaSensei",
+  },
+  icons: {
+    icon: "/images/icon.jpeg",
+  },
+  openGraph: {
+    title: "Home - Masayoshi",
+  },
+};
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
